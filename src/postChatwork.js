6df880e46d9af25288ea50f100c996b517efc5a7
naosim <nao_pillows@yahoo.co.jp>
2017-11-05 16:03:18 +0900

    postChatwork.js

---
+++ src/postChatwork.js
@@ -0,0 +1,30 @@
+function createChatworkClient(token) {
+  var header = {'X-ChatWorkToken' : token };
+  return {
+    postMessage: function(roomId, message) {
+      var url = "https://api.chatwork.com/v2/rooms/" + roomId + "/messages";
+      var payload = {
+        'body': message
+      };
+      var options = {
+        'method': 'post',
+        'headers': header,
+        'payload': payload
+      };
+      UrlFetchApp.fetch(url, options);
+    },
+    addTask: function(roomId, message, toId) {
+      var url = 'https://api.chatwork.com/v2/rooms/' + roomId + '/tasks';
+      var payload = {
+        'body': message,
+        'to_ids': toId
+      };
+      var options = {
+        'method': 'post',
+        'headers': header,
+        'payload': payload
+      };
+      UrlFetchApp.fetch(url, options);
+    }
+  }
+}
