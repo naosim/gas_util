25e1ec273284874819fecba9b300c4eae95835f1
naosim <nao_pillows@yahoo.co.jp>
2017-11-05 16:00:17 +0900

    add getListFromSheet.js

---
+++ src/getListFromSheet.js
@@ -0,0 +1,17 @@
+function getListFromSheet(sheetName) {
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = spreadsheet.getSheetByName(sheetName);
+  var range = sheet.getDataRange();
+  var map = range.getValues();
+  
+  var header = map[0];
+  var ary = [];
+  for(var row = 1; row < map.length; row++) {
+    var obj = {};
+    for(var clm = 0; clm < map[0].length; clm++) {
+      obj[header[clm].trim()] = map[row][clm];
+    }
+    ary.push(obj);
+  }
+  return ary;
+}
